2e5681f0d7efbce92b5c05bbb15eb6ced8711058
saragar2 <garciamiguelsara@gmail.com>
2025-02-12 20:35:15 +0100

    idk

--- philo/time.c
+++ philo/time.c
@@ -6,7 +6,7 @@
 /*   By: saragar2 <saragar2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 16:16:25 by saragar2          #+#    #+#             */
-/*   Updated: 2025/02/12 16:16:41 by saragar2         ###   ########.fr       */
+/*   Updated: 2025/02/12 16:31:07 by saragar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 u_int64_t	get_time(void)
 {
+	u_int64_t		time;
 	struct timeval	tv;
 
 	if (gettimeofday(&tv, NULL))
-		return (error("gettimeofday() FAILURE\n", NULL));
-	return ((tv.tv_sec * (u_int64_t)1000) + (tv.tv_usec / 1000));
+		print_error("Error in gettimeofday()");
+	time = (tv.tv_sec * (u_int64_t)1000) + (tv.tv_usec / 1000);
+	return (time);
 }
